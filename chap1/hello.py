1af766534bcc4ca7ab74cca61e774361aacb772a
Dennis Gutierrez <Dennisx.I.Gutierrez@intel.com>
2017-02-18 12:39:08 -0800

    adding chap1 folder and hello.py

---
+++ chap1/hello.py
@@ -0,0 +1,11 @@
+#This program says hello and asks for my name
+
+print('Hello world!')
+print('What is your name?') #ask for their name
+myName = input()
+print('Good 2 meat U,' + myName)
+print('The Length of your name is:')
+print(len(myName))
+print('What is your age?') # ask for age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
