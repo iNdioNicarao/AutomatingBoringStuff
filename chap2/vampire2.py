4cc5b5db20b643deefded0e10c8c3612d47cf2c8
Dennis Gutierrez <Dennisx.I.Gutierrez@intel.com>
2017-02-18 16:57:17 -0800

    adding chap2 folder and files made while reading

---
+++ chap2/vampire2.py
@@ -0,0 +1,8 @@
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('u r not Alice!')
+elif age > 100:
+    print('you are not ALice, gramma')
+elif age > 2000:
+    print('Unlike u Alice is not an immortal Vampyre!')
